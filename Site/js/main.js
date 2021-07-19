652f3df59f788493dd53ca7fdfe043c84d326301
doboscosmin <61190789+doboscosmin@users.noreply.github.com>
2021-07-19 12:04:15 +0300

    Add files via upload

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,7 +1,17 @@
 
 //Efectele din Header
-TweenMax.fromTo('#page-header h4 ', 0.6, { y: -80, opacity: 0 }, {y: 0, opacity:1, delay:0 });
-TweenMax.fromTo('#page-header h1 ', 0.8, { y: -80, opacity: 0 }, {y: 0, opacity:1, delay:0 });
-TweenMax.fromTo('#page-header p ', 1, { y: -80, opacity: 0 }, {y: 0, opacity:1, delay:0 });
-TweenMax.fromTo('#page-header .image-bot ', 0.8, { x:80, opacity: 0 }, {x: -20, opacity:1, delay:0 });
+TweenMax.fromTo('#page-header h4 ', 0.6, { y: -80, opacity: 0 }, {y: 0, opacity:1, delay:0.5 });
+TweenMax.fromTo('#page-header h1 ', 0.8, { y: -80, opacity: 0 }, {y: 0, opacity:1, delay:0.5 });
+TweenMax.fromTo('#page-header p ', 1, { y: -80, opacity: 0 }, {y: 0, opacity:1, delay:0.5 });
+TweenMax.fromTo('#page-header .image-bot ', 0.8, { x:80, opacity: 0 }, {x: -20, opacity:1, delay:0.5 });
 
+$('#skills .hov-eff').hover(function() {
+  TweenMax.to($(this), 0.5, { y:-10, scale: 1.03 });
+  TweenMax.to($(this), 0.5, { boxShadow: '0 0 1px rgba(0,0,0,0.36)'  });
+},
+                                  function() {
+  TweenMax.to($(this), 0.5, { y:0, scale: 1 });
+  TweenMax.to($(this), 0.5, { boxShadow: '0 0 1px rgba(0,0,0,0.06)'  });
+});
+
+var ctrl = new ScrollMagic.Controller();
