f14633fe366d721660e3ccf57b1fb94caf7aed7d
doboscosmin <61190789+doboscosmin@users.noreply.github.com>
2021-07-19 08:34:54 +0300

    Add files via upload

---
+++ Site/js/main.js
@@ -0,0 +1,7 @@
+
+//Efectele din Header
+TweenMax.fromTo('#page-header h4 ', 0.6, { y: -80, opacity: 0 }, {y: 0, opacity:1, delay:0 });
+TweenMax.fromTo('#page-header h1 ', 0.8, { y: -80, opacity: 0 }, {y: 0, opacity:1, delay:0 });
+TweenMax.fromTo('#page-header p ', 1, { y: -80, opacity: 0 }, {y: 0, opacity:1, delay:0 });
+TweenMax.fromTo('#page-header .image-bot ', 0.8, { x:80, opacity: 0 }, {x: -20, opacity:1, delay:0 });
+
